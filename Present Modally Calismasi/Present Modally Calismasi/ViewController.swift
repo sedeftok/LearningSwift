c9a7b7c137b72870ab4ead3730117b2b7285eca4
sedef tok <sedeftok03@gmail.com>
2023-11-20 23:08:07 +0300

    present modally

---
+++ Present Modally Calismasi/Present Modally Calismasi/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Present Modally Calismasi
+//
+//  Created by sedef tok on 11.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func goto2(_ sender: Any) {
+        
+        
+    }
+}
+
---
+++ Present Modally Calismasi/Present Modally Calismasi/ViewController2.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController2.swift
+//  Present Modally Calismasi
+//
+//  Created by sedef tok on 11.10.2023.
+//
+
+import UIKit
+
+class ViewController2: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    @IBAction func tiklanDismiss(_ sender: Any) {
+        
+        self.dismiss(animated: true)
+    }
+    
+   
+}
